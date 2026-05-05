dcb76f7032e79c3c452fc0e5837c1b0bc5a38bdb
jhonseloi <jhons.eloi@gmail.com>
2026-05-05 18:26:03 -0300

    atualizacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home({posts}: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    const posts = await getAllPosts('_sort=id:desc&_start=0&_limit=30')
+    const posts = (await getAllPosts()).slice(0, 30)
 
     return {
         props: { posts },
